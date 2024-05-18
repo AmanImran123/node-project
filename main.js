b5acbb1e1d0b3a9d9b40e6748b403a58bd0911b1
AmanImran123 <167163491+AmanImran123@users.noreply.github.com>
2024-05-19 00:10:46 +0500

    TODO LIST 4. Project

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
 import inquirer from "inquirer";
-let myBlance = 10000;
-let myPin = 1234;
-let pinAnswer = await inquirer.prompt([
-    {
-        name: "pin",
-        message: "enter your pin",
-        type: "number"
-    }
-]);
-if (pinAnswer.pin == myPin) {
-    console.log("correct pin");
-    let operationAns = await inquirer.prompt([
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
         {
-            name: "operation",
-            message: "please select option",
-            type: "list",
-            choices: ["withdraw", "check balance"]
+            name: 'todo',
+            type: 'input',
+            message: 'eid ki shopping kya kya ki hai?'
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: 'or kia khareeda?',
+            default: 'false'
         }
     ]);
-    console.log(operationAns);
-    if (operationAns.operation === " withdraw") {
-        let amountAns = await inquirer.prompt([
-            {
-                name: "amount",
-                message: "enter amount",
-                type: "number"
-            }
-        ]);
-        myBlance -= amountAns.amount;
-        console.log("your remaining balance is: " + myBlance);
-    }
-    else if (operationAns.operation === " check balance ") {
-        console.log("your remaining balance is: " + myBlance);
-    }
-}
-else {
-    console.log("wrong pin");
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
 }
