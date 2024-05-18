ba13e8064e992df7229f5e054509a3c8b34ca0fd
AmanImran123 <167163491+AmanImran123@users.noreply.github.com>
2024-05-19 00:13:59 +0500

    CURRENCY CONVERTER 5.Project

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-import inquirer from "inquirer";
-let todos = [];
-let condition = true;
-while (condition) {
-    let addTask = await inquirer.prompt([
-        {
-            name: 'todo',
-            type: 'input',
-            message: 'eid ki shopping kya kya ki hai?'
-        },
-        {
-            name: 'addMore',
-            type: 'confirm',
-            message: 'or kia khareeda?',
-            default: 'false'
-        }
-    ]);
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
+import inquirer from 'inquirer';
+let currencyconverter = {
+    'USD': {
+        "USA": 1,
+        "ERU": 0.92,
+        "PKR": 278
+    },
+    "ERU": {
+        "USA": 1.08,
+        "ERU": 1,
+        "PKR": 300
+    },
+    "PKR": {
+        "USA": 0.004,
+        "ERU": 0.003,
+        "PKR": 1
+    },
+};
+const answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: " enter your currency which you want to convert?",
+        type: "list",
+        choices: ["USD", "ERU", "PKR"]
+    },
+    {
+        name: "to",
+        message: " select which currency you convert?",
+        type: "list",
+        choices: ["USD", "ERU", "PKR"]
+    },
+    {
+        name: "amount",
+        message: " enter the amount which you want to convert?",
+        type: "number",
+        choices: ["USD", "ERU", "PKR"]
+    }
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    const result = currencyconverter[from][to] * amount;
+    console.log(` you converstion from ${from} to ${to} is ${amount}`);
+}
+else {
+    console.log("please enter the valid input");
 }
--- main.ts
+++ main.ts
@@ -1,26 +1,61 @@
-import inquirer from "inquirer"
-let todos = [];
-let condition = true;
+import {log} from 'console';
+import inquirer from 'inquirer'
+
+let currencyconverter ={
+ 'USD':{
+    "USA": 1,
+    "ERU": 0.92,
+    "PKR":278
+},
+"ERU":{
+    "USA": 1.08,
+    "ERU": 1,
+    "PKR":300
+},
+"PKR":{
+    "USA": 0.004,
+    "ERU": 0.003,
+    "PKR": 1
+},
+}
+
+const answer : {
+    from:"USD"|"ERU"|"PKR",
+    to:"USD"|"ERU"|"PKR",
+    amount:number
+} = await inquirer.prompt(
+   [
+    {
+        name:"from",
+        message: " enter your currency which you want to convert?",
+        type:"list",
+        choices:["USD","ERU","PKR"]
+    },
+    {
+        name:"to",
+        message: " select which currency you convert?",
+        type:"list",
+        choices:["USD","ERU","PKR"]
+    },
+    {
+        name:"amount",
+        message: " enter the amount which you want to convert?",
+        type:"number",
+        choices:["USD","ERU","PKR"]
+    }
+   ] 
+)
+
+const {from, to, amount} = answer;
+
+if(from && to && amount){
+    const result = currencyconverter[from][to] * amount;
+    console.log(` you converstion from ${from} to ${to} is ${amount}`)
+
+} else{
+    console.log("please enter the valid input")
+}
 
 
-while(condition)
- {
-    let addTask = await inquirer.prompt(
-        [
-            {
-                name: 'todo',
-                type: 'input',
-                message: 'eid ki shopping kya kya ki hai?'
-            },
-            {
-                name: 'addMore',
-                type: 'confirm',
-                message: 'or kia khareeda?'
-                default:'false'
-            }
-        ]
-    );
-    todos.push(addTask.todo);
-    condition = addTask.addMore;
-    console.log(todos);
- }   
+
+
